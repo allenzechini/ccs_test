5ca6420e588c17f4042330dcfd13ea42a088ae25
allenzechini <76968091+allenzechini@users.noreply.github.com>
2021-02-22 13:20:05 -0700

    Update urls.py
    - Removed commented code

--- root/config_compare/urls.py
+++ root/config_compare/urls.py
@@ -5,9 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('compare.html', views.CompareConfigView.as_view(), name='compare_config'),
-    # path('compare.html', views.compare_config, name='compare_config'),
     path('compare_success.html', views.compare_success, name='compare_success'),
     path('upload.html', views.UploadConfigView.as_view(), name='upload_config'),
-    # path('upload.html', views.upload_config, name='upload_config'),
     path('upload_success.html', views.upload_success, name='upload_success'),
 ]
