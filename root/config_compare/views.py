d0c289a9e8446557b5747ad82b695e18d1f1a38d
allenzechini <76968091+allenzechini@users.noreply.github.com>
2021-02-22 12:55:08 -0700

    Update views.py
    - Removing commented code
    - Moving functions that don't belong to classes at top of file

--- root/config_compare/views.py
+++ root/config_compare/views.py
@@ -11,6 +11,33 @@ from django.views.generic.edit import FormView
 from .models import ConfigurationFile
 from .forms import CompareConfigForm, UploadConfigForm
 
+def do_diff(file1, file2):
+    f1 = open(file1)
+    f2 = open(file2)
+    lines1 = f1.readlines()
+    lines2 = f2.readlines()
+    f2.close()
+    f1.close()
+    
+    diff_contents = difflib.HtmlDiff().make_file(
+        lines1, 
+        lines2,
+        os.path.basename(file1),
+        os.path.basename(file2),
+        context=True,
+        numlines=10
+    )
+    return diff_contents
+
+def handle_uploaded_file(f):
+    filename = settings.MEDIA_ROOT / 'uploads' / f.name
+    with open(filename, 'wb') as destination:
+        # using chunks to prevent FS from being overwhelmed by large files
+        # might be possible to do this in some validation somewhere else
+        for chunk in f.chunks():
+            destination.write(chunk)
+    destination.close()
+
 class CompareConfigView(FormView):
     form_class = CompareConfigForm
     template_name = "compare.html"
@@ -61,59 +88,3 @@ def compare_success(request):
 
 def upload_success(request):
     return render(request, 'upload_success.html')
-
-# def upload_config(request):
-#     if request.method == 'POST':
-#         form = UploadConfigForm(request.FILES)
-#         files = request.FILES.getlist('file_field')
-#         if form.is_valid():
-#             for f in files:
-#                 handle_uploaded_file(f)
-#                 model = ConfigurationFile(configuration_file=f)
-#                 model.save()
-#             return HttpResponseRedirect(reverse('upload_success'))
-#     else:
-#         form = UploadConfigForm()
-#     return render(request, 'upload.html', {'form': form})
-
-# def compare_config(request):
-#     return render(request, 'compare.html')
-
-def do_diff(file1, file2):
-    f1 = open(file1)
-    f2 = open(file2)
-    lines1 = f1.readlines()
-    lines2 = f2.readlines()
-    f2.close()
-    f1.close()
-    
-    diff_contents = difflib.HtmlDiff().make_file(lines1, lines2, 
-                                        os.path.basename(file1), os.path.basename(file2),
-                                        context=True, numlines=10)
-    return diff_contents
-
-    # filename1 = os.path.basename(file1)
-    # filename2 = os.path.basename(file2)
-    # filename1 = os.path.splitext(filename1)[0]
-    # filename2 = os.path.splitext(filename2)[0]
-    # diff_file = "diff_" + filename1 + "_" + filename2 + ".html"
-    # diff_file_full = settings.MEDIA_ROOT / 'diffs' / diff_file
-    # with open(diff_file_full, 'w') as output:
-    #     output.write(diff_contents)
-    # output.close()
-    # return diff_file
-    # return {
-    #     'diff_contents': diff_contents,
-    #     'diff_file': diff_file,
-    #     'diff_file_full': diff_file_full,
-    # }
-
-def handle_uploaded_file(f):
-    filename = settings.MEDIA_ROOT / 'uploads' / f.name
-    with open(filename, 'wb') as destination:
-        # using chunks to prevent FS from being overwhelmed by large files
-        # might be possible to do this in some validation somewhere else
-        for chunk in f.chunks():
-            destination.write(chunk)
-    destination.close()
-
