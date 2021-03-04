323851a6d16fc7cd27bb6465ba92655de576fd04
allenzechini <76968091+allenzechini@users.noreply.github.com>
2021-03-04 09:14:43 -0700

    Update settings.py
    Removing secret key

--- root/ccs_test/settings.py
+++ root/ccs_test/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#!fp&g0m_2__k(k9zta(602b^0d-=&d2v4ktf#hio1hh8%hlvx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
